90ddfad0569aef0070f97ec50bdc9bc5f8c54e71
jjurman <jjurman@constantcontact.com>
2018-05-19 22:21:07 -0400

    better webpack scheme

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path')
 
 module.exports = {
   entry: './main.js',
+  externals: {
+    domino: 'domino'
+  },
   output: {
     filename: 'bundle.js',
+    publicPath: './public',
     path: path.resolve(__dirname, 'dist')
   }
 }
